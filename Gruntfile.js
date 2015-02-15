49a8d8a85e28dbba1974b76c013499afa038dd20
rogeralsing <rogeralsing@gmail.com>
2015-02-15 10:02:13 +0100

    Got layouts working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,19 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        assemble: {
+            options: {
+                layout: "src/layouts/master.hbs",
+                flatten: true
+            },
+            pages: {
+                files: {
+                    'web/': ['src/*.hbs']
+                }
+            }
+        }
     });
     grunt.loadNpmTasks('assemble');
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['assemble']);
 };
