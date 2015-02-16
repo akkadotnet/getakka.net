5884c4655ae1d2b09ee6c20c1b0f558703b820cf
rogeralsing <rogeralsing@gmail.com>
2015-02-16 21:05:13 +0100

    Open web page

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,25 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        open : {
+          dev : {
+            path: 'http://127.0.0.1:8080/'
+          }
+        },
+        'http-server': {
+        'dev': {
+            root: "web",
+            port: 8080,
+            host: "0.0.0.0",
+            cache: 0,
+            showDir : true,
+            autoIndex: true,
+            // server default file extension
+            ext: "html",
+            // run in parallel with other tasks
+            runInBackground: false
+        }
+    },
         clean: ['akka.net',output,'src/wiki'],
         gitclone: {
             "akka.net": { //clone akka.net repository
@@ -19,7 +38,7 @@ module.exports = function(grunt) {
 
         copy: {
             "documentation" : { //copy documentation and assets from akka.net repo to our source folder
-                files: [ 
+                files: [
                     {
                         expand: true,
                         cwd   : 'akka.net/documentation',
@@ -40,10 +59,10 @@ module.exports = function(grunt) {
                 ]
             }
         },
-        replace: { //fix template names, that is, add .hbs extension 
+        replace: { //fix template names, that is, add .hbs extension
           "template names": {
-            src: ['src/**/*.md'], 
-            overwrite: true,  
+            src: ['src/**/*.md'],
+            overwrite: true,
             replacements: [{
               from: 'layout: wiki',
               to: 'layout: wiki.hbs'
@@ -61,23 +80,25 @@ module.exports = function(grunt) {
                 expand: true,
                 layoutdir: layouts,
                 helpers: ['helper-gfm.js'],
-                assets: assets 
+                assets: assets
             },
 
             "pages": { //build all pages, hbs and markdown
                 files: [
                     {
-                        expand: true, 
-                        cwd: source, 
-                        src: ['*.hbs','pages/*.hbs','**/*.md'], 
-                        dest: output, 
+                        expand: true,
+                        cwd: source,
+                        src: ['*.hbs','pages/*.hbs','**/*.md'],
+                        dest: output,
                         ext: '.html'
                     }
                 ]
             }
         }
     });
-    
+
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-http-server');
     grunt.loadNpmTasks('grunt-text-replace');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -89,6 +110,8 @@ module.exports = function(grunt) {
         'gitclone',         //fetch akka.net
         'copy',             //copy documentation to src, copy resources from src to output
         'replace',          //fix up template names (.hbs)
-        'assemble']);       //build pages
+        'assemble',         //build pages
+        'open',
+        'http-server'       //start server
+        ]);
 };
-
